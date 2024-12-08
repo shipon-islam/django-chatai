1263a14e465634e456d56896237b9c96fbf28a20
shipon islam <shiponkhan566@gmail.com>
2024-12-08 16:53:28 +0600

    modifiered vercel.json file2

--- chatai/settings.py
+++ chatai/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media"
--- vercel_build.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import sys
-
-# Ensure collectstatic is run during deployment
-subprocess.check_call([sys.executable, "manage.py",
-                      "collectstatic", "--noinput"])
