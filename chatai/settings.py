a6c80dea563ffd2f1f7f27d5fc377b7745bb28b9
shipon islam <shiponkhan566@gmail.com>
2024-12-10 15:39:47 +0600

    update vercel.json file3

--- chatai/settings.py
+++ chatai/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media"
---
+++ staticfiles/js/chat.js
@@ -0,0 +1,74 @@
+const formUser = document.getElementById("chat-form");
+const inputUser = document.getElementById("user-input");
+const messageContainer = document.getElementById("message-container");
+const chatContainer = document.getElementById("chat-container");
+
+const createMessageUi = (message) => {
+  const div = document.createElement("div");
+  div.className = "mt-10 space-y-3";
+  div.id = "chat-container";
+  div.innerHTML = `<div  class="flex items-center gap-x-2">
+            <img
+              class="rounded-full w-8 h-8 object-cover border border-gray-500"
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=50"
+              alt=""
+            />
+            <p
+               class="dark:text-gray-100 dark:bg-gray-800 p-3 rounded-lg shadow max-w-md"
+            >
+             ${message.message}
+            </p>
+          </div>
+          <div class="flex flex-col">
+            <i class="ri-sparkling-line text-yellow-500 dark:text-yellow-400 text-2xl"></i>
+            <p class="dark:text-gray-100 p-3 rounded-lg max-w-md">
+              ${message.ai_message}
+            </p>
+          </div>
+       `;
+  messageContainer.appendChild(div);
+};
+
+inputUser.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    formUser.requestSubmit();
+  }
+});
+
+formUser.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const formData = new FormData(form);
+  const csrfToken = document.querySelector("[name=csrfmiddlewaretoken]").value;
+  const responseMessage = document.getElementById("response-message");
+
+  try {
+    const response = await fetch(form.action, {
+      method: "POST",
+      headers: {
+        "X-CSRFToken": csrfToken,
+      },
+      body: formData,
+    });
+
+    const data = await response.json();
+    console.log(data);
+    createMessageUi(data);
+    chatContainer.scrollTo({
+      top: chatContainer.scrollHeight,
+      behavior: "smooth",
+    });
+    form.reset();
+  } catch (error) {
+    responseMessage.textContent = "Network error: " + error.message;
+    responseMessage.style.color = "red";
+  }
+});
+
+window.onload = () => {
+  chatContainer.scrollTo({
+    top: chatContainer.scrollHeight,
+  });
+  inputUser.focus();
+};
